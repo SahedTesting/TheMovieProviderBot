48fc9add1cb11a4d4cd44fe6b6c42574fef65dd4
SahedTesting <shahedsarker965@gmail.com>
2024-12-02 23:27:14 +0300

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
 VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
-HOWTOVERIFY = environ.get('HOWTOVERIFY', url='https://t.me/How_To_Open_Gpliinks/6') # How to open tutorial link for verification
+HOWTOVERIFY = environ.get('HOWTOVERIFY')
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantearn.in')
